2a97a64dd56621614186c4e784f59c27343bee70
chemaT <chemaaa94@gmail.com>
2022-03-11 18:19:37 -0600

    SideBar

---
+++ setup/app.js
@@ -0,0 +1,7 @@
+const toggleBtn = document.querySelector(".sidebar-toggle");
+const closeBtn = document.querySelector(".close-btn");
+const sideBar = document.querySelector(".sidebar");
+
+toggleBtn.addEventListener("click", function(){
+    
+})
